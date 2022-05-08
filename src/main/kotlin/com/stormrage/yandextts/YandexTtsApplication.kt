0bfbf274cbd94951529949fa7ee19824dd991953
Andrey Svistukhin <a.svistukhin@just-ai.com>
2022-05-08 23:35:12 +0300

    Initial commit

---
+++ src/main/kotlin/com/stormrage/yandextts/YandexTtsApplication.kt
@@ -0,0 +1,11 @@
+package com.stormrage.yandextts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YandexTtsApplication
+
+fun main(args: Array<String>) {
+    runApplication<YandexTtsApplication>(*args)
+}
---
+++ src/test/kotlin/com/stormrage/yandextts/YandexTtsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stormrage.yandextts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YandexTtsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
